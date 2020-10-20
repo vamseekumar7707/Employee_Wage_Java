efb4bad3ca83313f83d894ff0a5d30ea2e3c15b9
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-20 21:07:33 +0530

    Added Switch Case

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -33,6 +33,43 @@ public class Employee_Wage {
             return "Employee is not a Part Time Employee ";
         }
     }
+   //use case 4
+ 	public static int generateWorkHours ( int randomValue ){
+         int workingHours = 0;
+         switch (randomValue){
+             case 0: 
+                  workingHours = 4;
+                 break;
+             case 1 :
+                  workingHours = 3;
+                 break;
+             case 2:
+                 workingHours = 5;
+                 break;
+             case 3:
+                 workingHours = 10;
+                 break;
+         }
+         return workingHours;
+     }
+ 	 public static int generateRandomWorkDays ( int randomValue ) {
+         int workingDays = 0;
+         switch (randomValue){
+             case 0: 
+                  workingDays = 20;
+                 break;
+             case 1 :
+                  workingDays = 8;
+                 break;
+             case 2:
+                 workingDays = 5;
+                 break;
+             case 3:
+                 workingDays = 1;
+                 break;
+         }
+         return workingDays;
+     }
 	 public static void main(String args[]) {
        
       System.out.println("Welcome To Employee Wage Calculation Problem");
@@ -49,7 +86,10 @@ public class Employee_Wage {
       //use case 3
       String wageForPartTimeEmp = calculateWageCalculationForPartTimeEmps();
       System.out.println("Employees Part Time Wage : "+wageForPartTimeEmp);
-
+    //use case 4
+  	int randomWorkHrs = generateWorkHours(randomValue+2);
+        System.out.println("Randomly generated Work Hours is : "+randomWorkHrs);
+      
       }
 	}
 }
