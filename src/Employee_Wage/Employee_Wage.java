0642f4dccaf0a081fe6aa8d8c91d4ef073580bd8
vamseekumar7707 <vamseekumar7707@gmail.com>
2020-10-20 20:40:40 +0530

    Added Check Employee Present or Absent

--- src/Employee_Wage/Employee_Wage.java
+++ src/Employee_Wage/Employee_Wage.java
@@ -1,7 +1,26 @@
 package Employee_Wage;
 public class Employee_Wage {
-         public static void main(String args[]) {
+	 public static int generateRandomNumber(){
+         double x=Math.random();
+        int y = (int) Math.round(x);
+        return y;
+   	 }
+	 public static String presenteeCheckForEmployee(int value){
+	        if(value==1){
+	            return "Employee is Present" ;
+	                    
+	        }else{
+	            return "Employee is Absent" ;
+	        }
+	    } 
+	 
+	 
+	 public static void main(String args[]) {
        
       System.out.println("Welcome To Employee Wage Calculation Problem");
-      }
+   // use case 1
+      int randomValue = generateRandomNumber();
+      String presenteeCheck = presenteeCheckForEmployee(randomValue);
+      System.out.println(presenteeCheck);
+	}
 }
